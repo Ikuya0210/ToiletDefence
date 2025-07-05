323f6e4abdea8839ff839c98a8ef4b9eb2a379b0
Ayuki Isogawa <ayumi50nozomi@gmail.com>
2025-07-05 16:42:15 +0900

    いらないフリッパーなどを削除

--- Assets/Scripts/Game/Root.cs
+++ Assets/Scripts/Game/Root.cs
@@ -8,12 +8,10 @@ namespace PinballBenki.Game
     internal sealed class Root : SceneRoot
     {
         protected override SceneNames SceneName => SceneNames.Game;
-        [SerializeField] private Flippers _flippers;
         [SerializeField] private GameGUIManager _guiManager;
 
         protected override UniTask InitBeforeShow(CancellationToken ct)
         {
-            _flippers.Init();
             _guiManager.Init();
             return UniTask.CompletedTask;
         }
